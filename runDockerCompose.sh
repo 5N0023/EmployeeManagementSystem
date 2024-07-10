46b2e599619ea8465ccf89ff9637eaffc982a1ab
5N0023 <lektaibim@gmail.com>
2024-07-10 16:45:44 +0100

    adding script for docker-compose and local running ( need to fix backend docker)

---
+++ runDockerCompose.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+
+# This script is used to run the program using Docker Compose
+docker-compose up --build
---
+++ runLocal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script is used to run the program locally
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+export PATH="$PATH:$HOME/.dotnet/tools"
+
+# Navigate to the backend project directory and run database update
+cd EmployeeManagementSystemAPI
+dotnet ef database update
+# kill any existing dotnet process
+pkill dotnet || true
+# Run the backend project
+dotnet run &
+
+# Navigate to the frontend project directory and run the Angular app
+cd ../EmployeeManagementSystemFront
+ng serve --open
